b74fb4ed92565ea8dd332b8068257d4b0762ac5c
Aleksandra Sliusar <oleksandra_sliusar@epam.com>
2020-03-14 16:19:13 +0200

    Task 4.2. Add CORS middleware

--- task2/lib/index.js
+++ task2/lib/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const cors = require('cors');
 const schema = require('./users.post.schema');
 const usercontroller = require('./usercontroller');
 const { logService, logError } = require('./logging');
 
 app.listen(3000);
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 router.post('/authenticate', usercontroller.login);
